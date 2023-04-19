8f6aea49cfd5b12e7fd7e1785ff8c41dde397715
TuhRel <tonytj0106@gmail.com>
2023-04-19 18:40:41 -0400

    added a textarea form field

--- app/components/form-field.tsx
+++ app/components/form-field.tsx
@@ -30,7 +30,36 @@ export function FormField({
     <input onChange={e => {
       onChange(e)
       setErrorText('')
-    }} type={type} id={htmlFor} name={htmlFor} className=' border rounded border-solid border-slate-900 p-1' value={value} />
+    }} type={type} id={htmlFor} name={htmlFor} className='border rounded border-solid border-slate-900 p-1' value={value} />
+
+    <div className='text-xs font-semibold text-center tracking-wide text-red-500 w-full'>
+      {errorText || ''}
+    </div>
+    </>
+  )
+}
+
+export function FormTextField({
+  htmlFor,
+  label,
+  value,
+  onChange = () => { },
+  error = ''
+}: FormFieldProps) {
+  const [ errorText, setErrorText ] = useState(error)
+
+  useEffect(() => {
+    setErrorText(error)
+  }, [error])
+
+  return (
+    <>
+    <label htmlFor={htmlFor} className='text-black font-light'>{label}</label>
+
+    <textarea onChange={e => {
+      onChange(e)
+      setErrorText('')
+    }} id={htmlFor} name={htmlFor} className='h-28 border rounded border-solid border-slate-900 p-1 overflow-scroll' value={value} />
 
     <div className='text-xs font-semibold text-center tracking-wide text-red-500 w-full'>
       {errorText || ''}
