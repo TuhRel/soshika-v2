be6990b53e3c31a65999694193c13f07e2efea3e
TuhRel <tonytj0106@gmail.com>
2023-04-19 18:43:41 -0400

    update loader function for porfolio section images

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,14 +5,16 @@ import HeroSection from "~/components/hero-section";
 import SubmenuBanner from "~/components/submenu-banner";
 import Submenu from "~/components/submenu";
 import PortfolioBanner from "~/components/portfolio-banner";
-import Portfolio from "~/components/portfolio";
+import Portfolio from "~/components/portfolio-section";
 import { getOtherUsers } from "~/utils/users.server";
 import { useLoaderData } from "@remix-run/react";
+import { generatePresignedUrlsForFolder } from "~/utils/s3.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request)
   const users = await getOtherUsers(userId)
-  return json({ users })
+  const images = await generatePresignedUrlsForFolder('home-page-images', 600)
+  return json({ users, images })
 }
 
 export default function Home() {
